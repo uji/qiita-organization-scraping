082b9d737d64f3dc94e8150516e45ecd4f7d1418
uji <ujiprog@gmail.com>
2020-02-22 23:03:28 +0900

    add chrome-aws-lambda

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
+const chromium = require("chrome-aws-lambda");
 const { App, ExpressReceiver } = require("@slack/bolt");
-const puppeteer = require("puppeteer");
 
 const receiver = new ExpressReceiver({
   signingSecret: process.env.SLACK_SIGNING_SECRET
@@ -10,60 +10,60 @@ const app = new App({
   receiver: receiver
 });
 
-const fs = require("fs");
-const path = "latest.text";
 const channel = process.env.SLACK_CHANNEL;
 
-let data;
-fs.readFile(path, "utf8", function (_, file) {
-  data = file;
-});
-
-(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto("http://qiita.com/organizations/" + process.argv[2]);
-  const selector = ".of-ItemLink_header-title";
-  let elems = await page.$$eval(selector, es => es.map(e => [e.textContent, e.href]));
-  await browser.close();
-
+exports.handler = async (event, context) => {
+  let browser = null;
   let posts = [];
-  elems.some(elem => {
-    if (elem[0] === data) {
-      return true;
-    }
-    posts.push(elem);
-  });
-  console.log(posts);
 
-  if (posts.length == 0) return;
+  try {
+    browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+    });
 
-  await app.client.chat.postMessage({
-    channel: channel,
-    blocks:
-      [
-        {
-          "type": "section",
-          "text": {
-            "type": "mrkdwn",
-            "text": "*Qiitaに新しい記事が投稿されました*"
-          }
-        }
-      ],
-    token: process.env.SLACK_BOT_TOKEN
-  });
+    let page = await browser.newPage();
 
-  await posts.forEach(post => {
-    app.client.chat.postMessage({
+    await page.goto("http://qiita.com/organizations/" + process.env.ORGANIZATION_NAME);
+    const selector = ".of-ItemLink_header-title";
+    let elems = await page.$$eval(selector, es => es.map(e => [e.textContent, e.href]));
+
+    await elems.some(elem => {
+      posts.push(elem);
+    });
+    if (posts.length == 0) return;
+
+    await app.client.chat.postMessage({
       channel: channel,
-      text: post[1],
+      blocks:
+        [
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "*Qiitaに新しい記事が投稿されました*"
+            }
+          }
+        ],
       token: process.env.SLACK_BOT_TOKEN
     });
-  });
 
-  fs.writeFile(path, elems[0][0], function (err) {
-    if (err) {
-        throw err;
+    await posts.forEach(post => {
+      app.client.chat.postMessage({
+        channel: channel,
+        text: post[1],
+        token: process.env.SLACK_BOT_TOKEN
+      });
+    });
+  } catch (error) {
+    return context.fail(error);
+  } finally {
+    if (browser !== null) {
+      await browser.close();
     }
-  });
-})();
+  }
+
+  return context.succeed(posts);
+};
